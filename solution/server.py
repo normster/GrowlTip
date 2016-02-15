8ece6490a6c1bc6448886435a9c147d600ee822a
Norman <thenormanmu@gmail.com>
2016-02-15 10:38:55 -0800

    incomplete

---
+++ solution/server.py
@@ -0,0 +1,68 @@
+import sqlite3
+import time
+from flask import Flask, request, g, render_template
+from blockchain import wallet
+
+app = Flask(__name__)
+DATABASE = 'growls.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def db_read_growls():
+    cur = get_db().cursor()
+    cur.execute("CREATE TABLE if not EXISTS growls (name TEXT, datetime TEXT, growl TEXT, wallet TEXT)")
+    cur.execute("SELECT * FROM growls")
+    return cur.fetchall()
+
+def db_add_growl(name, growl, wallet):
+    cur = get_db().cursor()
+    t = str(time.time())
+    growl_info = (name, t, growl, wallet)
+    cur.execute("INSERT INTO growls VALUES (?, ?, ?, ?)", growl_info)
+    get_db().commit()
+
+@app.route("/")
+def hello():
+    growls = db_read_growls()
+    print(growls)
+    return render_template('index.html', growls=growls)
+
+@app.route("/", methods=["POST"])
+def testing():
+    print(request.form)
+    db_add_growl(request.form['name'], request.form['growl'], request.form['wallet'])
+    return hello()
+
+
+@app.route("/api/growl", methods=["POST"])
+def receive_growl():
+    print(request.form)
+    db_add_growl(request.form['name'], request.form['growl'], request.form['wallet'])
+    return hello()
+    # TODO: redirect to "/"
+
+@app.route("/api/tip", methods=["POST"])
+def send_tip():
+    print(request.form)
+    identifier = request.form['identifier']
+    password = request.form['password']
+    wal = wallet.Wallet("{0}".format(identifier), "{0}".format(password))
+    
+
+@app.route("/tip")
+def tip_login():
+    return render_template('tip.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
